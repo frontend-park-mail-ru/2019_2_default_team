e92422b5032daf1a4499e4faee87369c9709dc35
dip <dmpuchenkov@protonmail.com>
2019-12-28 00:37:44 +0300

    sw

--- src/js/application.js
+++ src/js/application.js
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
    renderHTML();
 
     if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/sw.js')
+        navigator.serviceWorker.register('./sw.js')
             .then((reg) => {
                 // регистрация сработала
                 console.log('Registration succeeded. Scope is ' + reg.scope);
