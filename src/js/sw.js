556db0bc57cd242831a3797aa629ab75dcceef90
Valentine <baymakanov@gmail.com>
2019-12-29 16:01:22 +0300

    Maybe a fix

--- src/js/sw.js
+++ src/js/sw.js
@@ -15,10 +15,6 @@ self.addEventListener('fetch', (event) => {
     event.respondWith(
         caches.match(event.request)
             .then((cachedResponse) => {
-                if (cachedResponse && !navigator.onLine) {
-                    return cachedResponse;
-                }
-
                 return fetch(event.request)
                     .then((response) => {
                         return caches.open(cacheName).then((cache) => {
@@ -27,6 +23,10 @@ self.addEventListener('fetch', (event) => {
                             }
                             return response;
                         });
+                    }).catch(err => {
+                        if (cachedResponse) {
+                            return cachedResponse;
+                        }
                     });
             })
             .catch(() => {
